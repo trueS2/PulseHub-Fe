9414175102ecbc74a9c2ac910bec7a47052eb674
true <truee030@gmail.com>
2025-05-16 16:39:53 +0900

    chore: 초기 세팅 밑 환경 설정

---
+++ src/app/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css';
+
+
+function HomePage() {
+    return (
+        <div>
+            메인 페이지
+        </div>
+    );
+}
+
+export default HomePage;
