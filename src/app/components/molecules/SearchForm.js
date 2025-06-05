5622cacd2f3b5faaced44d040af7d9d931082187
true <truee030@gmail.com>
2025-06-05 17:15:23 +0900

    design: 검색창

--- src/app/components/molecules/SearchForm.js
+++ src/app/components/molecules/SearchForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import InputField from '../atoms/InputField';
-import Button from '../atoms/Button';
+import Button from '../atoms/SearchBtn';
 
 function SearchForm({ username, setUsername, onSearch }) {
     return (
