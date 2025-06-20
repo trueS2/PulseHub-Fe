dc43a3d57f2ea2b9cc4afbe899c92b436e73077f
true <truee030@gmail.com>
2025-06-20 16:39:57 +0900

    chore: 필요없는 주석 제거

--- src/app/components/organisms/UserProfile.js
+++ src/app/components/organisms/UserProfile.js
@@ -3,14 +3,13 @@ import Avatar from '../atoms/Avatar';
 import Heading from '../atoms/Heading';
 import Text from '../atoms/Text';
 import FollowInfo from '../atoms/FollowInfo';
-import styles from './UserProfile.module.css'; // ✅ CSS 모듈 추가
+import styles from './UserProfile.module.css';
 
 function UserProfile({ user }) {
     return (
         <div>
             <Heading level={3}>{user.login}</Heading>
 
-            {/* ✅ 아바타 + 팔로워 수를 가로로 묶기 */}
             <div className={styles.profileRow}>
                 <Avatar src={user.avatar_url} alt={`${user.login}'s avatar`} />
                 <div className={styles.profileInfo}>
