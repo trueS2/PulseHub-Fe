01adfb07885b84a6bdab9109613517393e1e0e8a
true <truee030@gmail.com>
2025-06-23 19:05:44 +0900

    feat: 로그인 페이지 레이아웃 개발

--- src/app/components/pages/LoginPage.js
+++ src/app/components/pages/LoginPage.js
@@ -1,15 +1,16 @@
-// src/app/components/pages/LoginPage.js
-import React from 'react';
-import Header from '../organisms/Header';
-import LoginSection from '../organisms/LoginSection';
+import React from 'react'
+import Header from '../organisms/Header'
+import LoginSection from '../organisms/LoginSection'
 
 function LoginPage() {
     return (
         <div>
             <Header />
-            <LoginSection />
+            <div className="flex-1">
+                <LoginSection />
+            </div>
         </div>
-    );
+    )
 }
 
-export default LoginPage;
+export default LoginPage
