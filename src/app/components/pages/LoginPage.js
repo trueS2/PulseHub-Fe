08671aa43a0758fc245d099456d7d4106a0e5aea
true <truee030@gmail.com>
2025-06-23 18:28:14 +0900

    feat: 헤더 추가

--- src/app/components/pages/LoginPage.js
+++ src/app/components/pages/LoginPage.js
@@ -1,9 +1,15 @@
 // src/app/components/pages/LoginPage.js
 import React from 'react';
+import Header from '../organisms/Header';
 import LoginSection from '../organisms/LoginSection';
 
 function LoginPage() {
-    return <LoginSection />;
+    return (
+        <div>
+            <Header />
+            <LoginSection />
+        </div>
+    );
 }
 
 export default LoginPage;
